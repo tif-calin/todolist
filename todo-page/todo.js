fb2499002a054f4f639378bf33076e81c25778e5
tif-calin <culitif@tuta.io>
2021-04-14 15:46:32 -0700

    add task to state

--- todo-page/todo.js
+++ todo-page/todo.js
@@ -1,3 +1,18 @@
+import { getUser } from '../local-storage.js';
 
 const form = document.querySelector('#make-task');
 const taskList = document.querySelector('#todo-list');
+
+const user = getUser();
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    const newTask = {
+        task: formData.get('write-task'),
+        isComplete: false
+    };
+
+    user.tasks.push(newTask);
+});
