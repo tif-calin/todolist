010a358e8a7f7cdfa65ed7106db103a3b370fd43
tif-calin <culitif@tuta.io>
2021-04-15 09:58:56 -0700

    add support for multiple users

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import { setUser, getUser, logIn } from './local-storage.js';
 
-
 const submit = document.getElementById('sign-in');
 
 submit.addEventListener('submit', (e) => {
@@ -10,25 +9,12 @@ submit.addEventListener('submit', (e) => {
     const username = formData.get('name');
     const password = formData.get('password');
 
-    if (getUser().name === username) { 
-        if (getUser().password === password) {
-            logIn(username);
-        } else {
-            console.log('Wrong pass');
-            return false;
-        }
-    } else { 
-        const user = {
-            name: username,
-            password: password,
-            tasks: []
-        };
-    
-        setUser(user);
-        
+    if (!getUser(username).name) {
+        console.log('No such user exists!');
+    } else if (getUser(username).password === password) { 
         logIn(username);
+        window.location = './todo-page';
+    } else {
+        console.log('Wrong password!');
     }
-
-    window.location = './todo-page';
-
 });
